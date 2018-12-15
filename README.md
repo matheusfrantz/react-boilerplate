266ecb7bbb06fbaee503783f2cdb935ff530a651
Frantz, Matheus (ESI) <matheus.frantz@adp.com>
2018-12-14 23:37:02 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# React boilerplate
+
+A ready to use React boilerplate with minimal webpack configuration.
+
+## Installation
+
+```
+npm install
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+}
