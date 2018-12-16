707f06695650772ccd45020c0718ae4e9db6a2e5
Frantz, Matheus (ESI) <matheus.frantz@adp.com>
2018-12-16 20:30:17 -0200

    Adding props on welcome component

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -4,7 +4,7 @@ class Welcome extends React.Component {
     render() {
         return (
             <div>
-                <h1>Welcome!</h1>
+                <h1>Welcome, {this.props.name}!</h1>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react"
 import ReactDOM from "react-dom"
 import Welcome from "./components/welcome"
 
-ReactDOM.render(<Welcome />, document.getElementById("welcome"))
+ReactDOM.render(<Welcome name="John" />, document.getElementById("welcome"))
